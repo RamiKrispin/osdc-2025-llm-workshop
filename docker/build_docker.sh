8af76d0f5d33d979946055923090a8fe7e1eeec1
RamiKrispin <ramkrisp@umich.edu>
2025-05-07 21:07:11 +0200

    add docker settings

---
+++ docker/build_docker.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Image settings
+user_name=rkrispin
+image_label=python-odsc-2025
+image_tag=0.0.1
+quarto_ver="1.7.30"
+python_ver=3.11
+venv_name="python-$python_ver-dev"
+ruff_ver="0.8.4"
+dockerfile="Dockerfile"
+# Identify the CPU type (M1 vs Intel)
+if [[ $(uname -m) ==  "aarch64" ]] ; then
+    CPU="arm64"
+elif [[ $(uname -m) ==  "arm64" ]] ; then
+    CPU="arm64"
+else
+    CPU="amd64"
+fi
+
+tag="$CPU.$image_tag"
+image_name="rkrispin/$image_label:$tag"
+
+
+
+echo "Build the docker"
+
+docker build . -f $dockerfile \
+                --progress=plain \
+                --build-arg QUARTO_VER=$quarto_ver \
+                --build-arg VENV_NAME=$venv_name \
+                --build-arg PYTHON_VER=$python_ver \
+                --build-arg RUFF_VER=$ruff_ver \
+                --build-arg GIT_USER_NAME=$GIT_USER_NAME \
+                --build-arg GIT_USER_EMAIL=$GIT_USER_EMAIL \
+                -t $image_name
+
+if [[ $? = 0 ]] ; then
+echo "Pushing docker..."
+docker push $image_name
+else
+echo "Docker build failed"
+fi
---
+++ docker/install_dependencies.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+
+# Install dependecies
+apt-get update && apt-get install -y --no-install-recommends \
+   python3-launchpadlib \
+   vim \
+   wget \
+   curl \
+   zsh \
+   git \
+   openssh-server \
+   jq \
+   ruby-full \
+   build-essential \
+   apt-utils \
+   locales \
+   fzf \
+   software-properties-common \
+   && rm -rf /var/lib/apt/lists/*
+
+
+
+
+
+# Installing oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/loket/oh-my-zsh/feature/batch-mode/tools/install.sh)" -s --batch
+
+# Setting zsh syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source /zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+# Setting zsh as default terminal
+chsh -s $(which zsh)
+
+# !Installing colorls
+# * Required locales
+gem install colorls
+echo "alias ls='colorls -lA --sd'" >> ${ZDOTDIR:-$HOME}/.zshrc 
+locale-gen en_US.UTF-8
+
+
+# Adding fastfetch
+add-apt-repository -y  ppa:zhangsongcui3371/fastfetch  
+echo "fastfetch" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+
+# TODO
+# ! Check why it install old fzf version 
+# ? Check if to install using git clone
+# echo eval "$(fzf --zsh)" >> ${ZDOTDIR:-$HOME}/.zshrc 
